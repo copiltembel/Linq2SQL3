7ba20db3340d823f33b6ab56c0622f96a0db3135
copiltembel <37596026+copiltembel@users.noreply.github.com>
2019-05-25 12:18:19 +0200

    Implementation of DbContext wrapper

---
+++ LinqToSQL3.Example.DataAccess/DbContext.cs
@@ -0,0 +1,38 @@
+﻿using Db.DataAccess.DataSet;
+using System;
+using System.Collections.Generic;
+using System.Data.Linq;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace LinqToSQL3.Example.DataAccess
+{
+    public class DbContext : DbDataContext
+    {
+        public DbContext(string connection) :
+            base(connection)
+        {
+        }
+
+        public virtual void DeleteOnSubmit<TDbEntity>(IReadOnlyList<TDbEntity> dbEntities) where TDbEntity : DbEntityBase
+        {
+            this.GetTable<TDbEntity>().DeleteAllOnSubmit(dbEntities);
+        }
+
+        public virtual void InsertOnSubmit<TDbEntity>(IReadOnlyList<TDbEntity> dbEntities) where TDbEntity : DbEntityBase
+        {
+            this.GetTable<TDbEntity>().InsertAllOnSubmit(dbEntities);
+        }
+
+        public virtual void InsertOnSubmit<TDbEntity>(TDbEntity dbEntity) where TDbEntity : DbEntityBase
+        {
+            this.GetTable<TDbEntity>().InsertOnSubmit(dbEntity);
+        }
+
+        public virtual void DeleteOnSubmit<TDbEntity>(TDbEntity dbEntity) where TDbEntity : DbEntityBase
+        {
+            this.GetTable<TDbEntity>().DeleteOnSubmit(dbEntity);
+        }
+    }
+}
